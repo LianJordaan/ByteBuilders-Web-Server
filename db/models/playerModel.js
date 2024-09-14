b9be00c4d99888387a3ebc4515e38ca429e07320
LianJordaan <lianjordaan30052005@gmail.com>
2024-09-14 04:51:26 +0200

    Added user Model for database

---
+++ db/models/playerModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+    uuid: { type: String, required: true },
+    username: { type: String, required: true },
+    favorites: { type: [Number], default: [] },
+    lastLogin: { type: Date },
+    firstLogin: { type: Date },
+    settings: { type: Map, of: String },
+    preferences: { type: Map, of: String },
+    tokens: { type: Number },
+    bytes: { type: Number },
+    plotSizes: {
+        128: { type: Number, default: 0 },
+        256: { type: Number, default: 0 },
+        512: { type: Number, default: 0 },
+        1024: { type: Number, default: 0 },
+        2048: { type: Number, default: 0 },
+        0: { type: Number, default: 0 }
+    }
+});
+
+const Player = mongoose.model('Player', playerSchema);
+
+module.exports = Player;
